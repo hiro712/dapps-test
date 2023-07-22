7aacf66ed8c5031e4c7ff83870ca5cc2b092ebf7
Hiroto Abe <abe15263076@gmail.com>
2023-07-22 17:29:06 +0900

    change dist

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  distDir: "build",
+  // distDir: "build",
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
